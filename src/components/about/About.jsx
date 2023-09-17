bc9a4d2d3b42275330b33907cdb4d29a35184c6f
jcala09 <b.cala@ttu.edu>
2023-09-16 20:23:11 -0500

    fadsafdsadsf

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,9 +6,9 @@ import {BiMedal} from 'react-icons/bi'
 const About = () => {
   return (
     <section id='about'>
-      <div className="container about__title">
       <h5>Who am I?</h5>
       <h2>About Me</h2>
+      <div className="container about__title">
       </div>
       <div className="container about__container">
         <div className="about__me">
