417b4a25735a1938be70a37f9504e20165651595
jcala09 <b.cala@ttu.edu>
2023-09-16 19:03:57 -0500

    added grad pic

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Header.css'
 import CTA from './CTA'
-import ME from '../../assets/about_me_image.jpg'
+import ME from '../../assets/gradpic.jpg'
 import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
