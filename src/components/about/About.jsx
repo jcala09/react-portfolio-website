70141dc61a1735b74648ea11e6320eb58775e5e5
jcala09 <jeromecala93@gmail.com>
2024-01-06 15:49:08 -0600

    rm summary, fix format

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,51 +7,55 @@ const About = () => {
   return (
     <section id='about'>
       <div className="about__header">
+
       <h5>Who am I?</h5>
       <h2>About Me</h2>
+
       </div>
+
       <div className="container about__title">
       </div>
+
       <div className="container about__container">
+
         <div className="about__me">
+
           <div className="about__me-pic">
+
             <img src={PICTURE} alt="About" />
+
           </div>
+
         </div>
+
         <div className= "about__content">
-        <div className="about__cards">
 
-          <article className='about__card'>
-          <BiMedal className='about__icon'/>
-          <h5>Project Experience</h5>
-          <small>5+ years of group and personal projects </small>
-          </article>
+          <div className="about__cards">
 
-          <article className='about__card'>
-          <BiMedal className='about__icon'/>
-          <h5>Work Experience</h5>
-          <small>1+ years of Software Engineering</small>
-          </article>
+            <article className='about__card'>
+            <BiMedal className='about__icon'/>
+            <h5>Project Experience</h5>
+            <small>5+ years of group and personal projects </small>
+            </article>
 
-          <article className='about__card'>
-          <BiMedal className='about__icon'/>
-          <h5>Social Experience</h5>
-          <small>Work very well in AGILE and group based environment </small>
-          </article>
-        
+            <article className='about__card'>
+            <BiMedal className='about__icon'/>
+            <h5>Work Experience</h5>
+            <small>1+ years of Software Engineering</small>
+            </article>
+
+            <article className='about__card'>
+            <BiMedal className='about__icon'/>
+            <h5>Social Experience</h5>
+            <small>Work very well in AGILE and group based environments </small>
+            </article>
           
+          </div>
 
-        </div>
         <div>
-        <div className= 'summary'>
-          My name is Jerome Gabriel Cala, and I recently graduated with a B.S. for Comptuer Science in Texas Tech University. I am born from the 
-          Philippines, and I moved here in the United States in the year of 2013. I have chosen Computer Science to be my major because I have 
-          always had a passion for computers, especially how video games and websites were made. I now work as an Associate Software Engineer 
-          for L3Harris, and continue to strive. My goal now is to be a future entrepeneur, with the hopes of making my own tech company.          
-          In my years as a Computer Science student, I have worked on projects such as a Public Transit Application through Object Oriented 
-          Programming, games such as space invaders and turtle racing. I am currently working with Luis Garcia and Gabe Guzman to create our own
-          Gym Clothing line called HNGR. More details will be released soon.
-        </div> 
+
+        
+
         </div>
         
         </div>
