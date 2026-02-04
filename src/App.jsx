3407111e087e009e0eff3ba8cc5801a68f7b752e
Jennifer Cala <jencala@ttu.edu>
2026-02-03 23:30:11 -0600

    remove contact import (unused)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Portfolio from './components/portfolio/Portfolio'
 import Services from './components/services/Services'
-import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
 const App = () => {
